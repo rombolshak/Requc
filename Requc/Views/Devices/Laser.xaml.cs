f8662f7b3ff6307f764e43b63022abcf077bba06
rombolshak <rombolshak@russia.ru>
2014-03-27 07:28:04 +0400

    Laser and phase devices views

---
+++ Requc/Views/Devices/Laser.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Requc.Views.Devices
+{
+    /// <summary>
+    /// Interaction logic for Laser.xaml
+    /// </summary>
+    public partial class Laser : UserControl
+    {
+        public Laser()
+        {
+            InitializeComponent();
+        }
+    }
+}
