1c59db22bea42d3d78bca1df716ef80920b13d31
rombolshak <rombolshak@russia.ru>
2015-03-01 23:15:56 +0300

    added untracked files

---
+++ Cascade/View/AliceBlockSetView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Cascade.View
+{
+    /// <summary>
+    /// Interaction logic for AliceBlockSetView.xaml
+    /// </summary>
+    public partial class AliceBlockSetView : UserControl
+    {
+        public AliceBlockSetView()
+        {
+            InitializeComponent();
+        }
+    }
+}
